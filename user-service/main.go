5c3883c48056f33efdb842d572db421c2f969cd3
quocvinh.nguyen <quocvinh.nguyen@grabtaxi.com>
2019-07-03 14:46:55 +0700

    adding db storage and apply docker compose

--- user-service/main.go
+++ user-service/main.go
@@ -15,43 +15,31 @@ const (
 )
 
 func main() {
-
 	// Database host from the environment variables
 	host := os.Getenv("DB_HOST")
-
 	if host == "" {
 		host = defaultHost
 	}
-
 	session, err := CreateSession(host)
-
 	// Mgo creates a 'master' session, we need to end that session
 	// before the main function closes.
 	defer session.Close()
-
 	if err != nil {
-
 		// We're wrapping the error returned from our CreateSession
 		// here to add some context to the error.
 		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
 	}
-
 	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
-
 		// This name must match the package name given in your protobuf definition
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
 	)
-
 	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
-
 	// Init will parse the command line flags.
 	srv.Init()
-
 	// Register handler
 	pb.RegisterShippingServiceHandler(srv.Server(), &service{session, vesselClient})
-
 	// Run the server
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
